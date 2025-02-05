36c393122dad2870ab3871772920b668d35d28b8
elucid503 <elucid@duck.com>
2025-02-05 15:05:15 -0500

    Add testing for linkedList basic ops

--- linkedList/LinkedListTester.java
+++ linkedList/LinkedListTester.java
@@ -7,7 +7,35 @@ public class LinkedListTester {
 		// create implementation, then...
 		
 		LinkedList tester = new LinkedListImpl();
+
 		tester.addItem("an item to add");
+		tester.addItem("another item to add");
+
+		System.out.println("Item count: " + tester.itemCount());
+
+		tester.listItems();
+
+		System.out.println("Item in list? " + tester.isItemInList("an item to add"));
+
+		System.out.println("Item in list? " + tester.isItemInList("not in list"));
+
+		tester.deleteItem("an item to add");
+
+		System.out.println("Item count: " + tester.itemCount());
+
+		tester.listItems();
+
+		boolean returnType = tester.deleteItem("not in list");
+
+		System.out.println("Item not in list, delete return: " + returnType);
+
+		tester.addItem("an item to add");
+
+		tester.addItem("another item to add");
+
+		tester.addItem("a third item to add");
+
+		tester.listItems();
 		
 	}
 
