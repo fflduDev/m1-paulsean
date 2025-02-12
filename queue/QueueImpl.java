a6d4febca5904d54de233fa48171b07dba933240
SeanSalafia <sean.salafia@student.fairfield.edu>
2025-02-12 15:05:23 -0500

    Queue work

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -2,28 +2,76 @@ package queue;
 
 public class QueueImpl implements Queue {
 
+	int length = 10;
+	String[] Queue = new String[length];
+
+
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		if (Queue[length - 1] != null) {
+			return false;
+
+		}
+
+		return true;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if (Queue[0] == null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void enqueue(String element) {
-		// TODO Auto-generated method stub
+
+		if (this.isFull()) {
+
+			return;
+
+		}
+	
+		if (Queue[0] == null) {
+
+			Queue[0] = element;
+			return;
+
+		} 
+
+		int i = 0;
 		
+		while (Queue[i] != null) {
+
+			i++;
+
+		}
+
+		Queue[i] = element;
+		
+	
 	}
 
 	@Override
 	public String dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (this.isEmpty()) {
+
+			return null;
+
+		}
+
+		String returnedString = Queue[0];
+
+		for (int i = 0; i < Queue.length - 1; i++) {
+
+			Queue[i + 1] = Queue[i];
+
+		}
+		
+		return returnedString;
+
 	}
 
 	@Override
--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -4,6 +4,18 @@ public class QueueTester {
 
 	public static void main(String[] args) {
 
+		Queue queueInstance = new QueueImpl();
+
+		queueInstance.enqueue("first");
+		queueInstance.enqueue("second");
+		queueInstance.enqueue("third");
+		queueInstance.enqueue("fourth");
+
+		queueInstance.dequeue();
+		queueInstance.dequeue();
+		queueInstance.dequeue();
+		queueInstance.dequeue();
+
 	}
 
 }
